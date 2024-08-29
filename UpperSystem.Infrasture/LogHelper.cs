b740bf44ba474699eb2f4ec2e4de2303b2736e41
Your Name <you@example.com>
2024-08-29 09:21:25 +0800

    Init

---
+++ UpperSystem.Infrasture/LogHelper.cs
@@ -0,0 +1,67 @@
+﻿using log4net.Config;
+using System;
+using System.IO;
+
+namespace UpperSystem.Infrasture
+{
+    public class LogHelper
+    {
+        #region 配置方法，加载配置文件
+        public static void SetConfig()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        //【程序窗体加载的时候调用这个方法】
+        public static void SetConfig(FileInfo configFile)
+        {
+            XmlConfigurator.Configure(configFile);
+        }
+
+        #endregion
+
+        #region 写入日志的方法
+
+        public static readonly log4net.ILog LogForFatal = log4net.LogManager.GetLogger("fatal");
+        public static readonly log4net.ILog LogForError = log4net.LogManager.GetLogger("error");
+        public static readonly log4net.ILog LogForWarn = log4net.LogManager.GetLogger("warn");
+        public static readonly log4net.ILog LogForInfo = log4net.LogManager.GetLogger("info");
+        public static readonly log4net.ILog LogForDebug = log4net.LogManager.GetLogger("debug");
+
+        public static void Fatal(string fatal, Exception e)
+        {
+            LogForInfo.Fatal(fatal, e);
+        }
+        public static void Error(string error_info, Exception t)
+        {
+            LogForError.Error(error_info, t);
+        }
+        public static void Warn(string warn_info)
+        {
+            LogForDebug.Warn(warn_info);
+        }
+        public static void Info(string info)
+        {
+            LogForInfo.Info(info);
+        }
+        public static void Debug(string debug_info)
+        {
+            LogForInfo.Debug(debug_info);
+        }
+
+        #endregion
+
+        #region 写入日志的方法【二】
+
+        public static void WriteLogInfo(string info)
+        {
+            bool isInfoEnabled = LogHelper.LogForInfo.IsInfoEnabled;
+            if (isInfoEnabled)
+            {
+                LogHelper.LogForInfo.Info(info);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ UpperSystem/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using UpperSystem.Infrasture;
+using IO = System.IO;
+
+namespace UpperSystem
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string fileName = IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log4net.config");
+            if (!File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, $"日志配置文件不存在,请检查文件路径\n,{fileName}");
+            }
+                
+            LogHelper.SetConfig(new FileInfo(fileName));
+        }
+    }
+}
